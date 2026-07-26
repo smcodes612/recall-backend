80f0cece11d2a20052086ce8895b1ce91941ec91
coolhacker <shrutibmishra1@gmail.com>
2026-07-26 17:39:38 -0400

    Describe your changes here

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ load_dotenv()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+    "https://recall-clpj.vercel.app",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
